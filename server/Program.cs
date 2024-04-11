1638468ab67a72655a564650a9b079e1b5bdf003
Jason Beres <jasonberes@gmail.com>
2024-04-10 21:24:10 -0400

    Add files via upload

---
+++ server/Program.cs
@@ -0,0 +1,144 @@
+using Reveal.Sdk;
+using Reveal.Sdk.Dom;
+using RevalSdk.Server;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers().AddReveal(builder => { });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+      builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    );
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "Images")),
+    RequestPath = "/Images"
+});
+
+app.MapGet("/dashboards/{name}/exists", (string name) =>
+{
+    var filePath = Path.Combine(Environment.CurrentDirectory, "Dashboards");
+    return File.Exists($"{filePath}/{name}.rdash");
+});
+
+app.MapGet("dashboards/visualizations", () =>
+{
+    try
+    {
+        var allVisualizationChartInfos = new List<VisualizationChartInfo>();
+        var dashboardFiles = Directory.GetFiles("Dashboards", "*.rdash");
+
+        foreach (var filePath in dashboardFiles)
+        {
+            try
+            {
+                var document = RdashDocument.Load(filePath);
+                foreach (var viz in document.Visualizations)
+                {
+                    try
+                    {
+                        var chartInfo = new VisualizationChartInfo
+                        {
+                            DashboardFileName = Path.GetFileNameWithoutExtension(filePath),
+                            DashboardTitle = document.Title,
+                            VizId = viz.Id,
+                            VizTitle = viz.Title,
+                            VizChartType = viz.ChartType.ToString(),                           
+                        };
+                        allVisualizationChartInfos.Add(chartInfo);
+                    }
+                    catch (Exception vizEx)
+                    {
+                        Console.WriteLine($"Error processing visualization {viz.Id} in file {filePath}: {vizEx.Message}");
+                    }
+                }
+            }
+            catch (Exception fileEx)
+            {
+                Console.WriteLine($"Error processing file {filePath}: {fileEx.Message}");
+            }
+        }
+        return Results.Ok(allVisualizationChartInfos);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem($"An error occurred: {ex.Message}");
+    }
+
+}).Produces<IEnumerable<VisualizationChartInfo>>(StatusCodes.Status200OK)
+  .Produces(StatusCodes.Status500InternalServerError);
+
+app.MapGet("/dashboards/{name}/thumbnail", async (string name) =>
+{
+    var path = "dashboards/" + name + ".rdash";
+    if (File.Exists(path))
+    {
+        var dashboard = new Dashboard(path);
+        var info = await dashboard.GetInfoAsync(Path.GetFileNameWithoutExtension(path));
+        return TypedResults.Ok(info);
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
+
+app.MapGet("/dashboards/names", () =>
+{
+    try
+    {
+        string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Dashboards");
+        var files = Directory.GetFiles(folderPath);
+        Random rand = new();
+
+        var fileNames = files.Select(file =>
+        {
+            try
+            {
+                return new DashboardNames
+                {
+                    DashboardFileName = Path.GetFileNameWithoutExtension(file),
+                    DashboardTitle = RdashDocument.Load(file).Title
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Reading FileData {file}: {ex.Message}");
+                return null;
+            }
+        }).Where(fileData => fileData != null).ToList();
+
+        return Results.Ok(fileNames);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error Reading Directory : {ex.Message}");
+        return Results.Problem("An unexpected error occurred while processing the request.");
+    }
+
+}).Produces<IEnumerable<DashboardNames>>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound)
+.ProducesProblem(StatusCodes.Status500InternalServerError);
+
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
