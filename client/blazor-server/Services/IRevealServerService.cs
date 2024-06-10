b6919dbe85ccae5375b2aa0b96dcc756045aaf84
Jason Beres <jasonberes@gmail.com>
2024-06-09 22:26:05 -0400

    Delete client/blazor-server directory

--- client/blazor-server/Services/IRevealServerService.cs
+++
@@ -1,9 +0,0 @@
-using DashboardViewer.Models.RevealServer;
-
-namespace DashboardViewer.RevealServer
-{
-    public interface IRevealServerService
-    {
-        Task<List<DashboardNames>> GetDashboardNamesList();
-    }
-}
